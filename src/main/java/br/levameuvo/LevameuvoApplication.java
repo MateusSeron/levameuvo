5730aa49309b0a160592206f5500cd6bc59bcda0
MateusSeron <mateus.seron@hotmail.com.br>
2017-11-10 23:04:39 -0200

    levameuvo
    initial commit

---
+++ src/main/java/br/levameuvo/LevameuvoApplication.java
@@ -0,0 +1,12 @@
+package br.levameuvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevameuvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevameuvoApplication.class, args);
+	}
+}
